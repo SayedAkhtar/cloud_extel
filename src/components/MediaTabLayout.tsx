4c7842e0dfe5f4aef53c09456bbdbc293f1ed643
Sayed Akhtar(Mac M1) <sayed182@gmail.com>
2025-03-24 12:47:11 +0530

    changes

--- src/components/MediaTabLayout.tsx
+++ src/components/MediaTabLayout.tsx
@@ -2,12 +2,11 @@ import { useState, type Key } from "react";
 
 const MediaTabLayout = ({ mediaData, pressData }: any) => {
   const [activeTab, setActiveTab] = useState("news");
-
   return (
     <>
-      <main className="w-screen h-auto min-h-[340px] relative mt-[80px] md:mt-[120px] bg-[url('/images/blogs/bg.jpeg')] bg-cover flex items-center">
-        <div className="w-full px-[20px] py-[20px] md:py-none md:px-[100px] order-2 md:order-1 container">
-          <h1 className="text-white text-[24px] md:text-[36px] font-bold">
+      <main className="relative mt-[80px] flex h-auto min-h-[340px] w-screen items-center bg-[url('/images/blogs/bg.jpeg')] bg-cover md:mt-[120px]">
+        <div className="md:py-none container order-2 w-full px-[20px] py-[20px] md:order-1 md:px-[100px]">
+          <h1 className="text-[24px] font-bold text-white md:text-[36px]">
             {activeTab === "news"
               ? "Monetising 5G will be a challenge but huge thirst for connectivity at home: Kunal Bajaj CEO & Co-Founder of CloudExtel"
               : "CloudExtel garners external credit rating of CARE A- for its debut debt fundraise of INR 200 cr. from NIIF IFL and ABFL"}
@@ -19,7 +18,7 @@ const MediaTabLayout = ({ mediaData, pressData }: any) => {
                 : "https://www.linkedin.com/feed/update/urn:li:activity:7188756231702921217"
             }
             target="_blank"
-            className="group flex w-fit gap-2 items-center font-medium bg-white border border-white rounded-full px-6 py-4 mt-6 hover:bg-inherit hover:text-white group-hover:fill-white transition"
+            className="group mt-6 flex w-fit items-center gap-2 rounded-full border border-white bg-white px-6 py-4 font-medium transition hover:bg-inherit hover:text-white group-hover:fill-white"
           >
             Know More
             <svg
@@ -31,7 +30,7 @@ const MediaTabLayout = ({ mediaData, pressData }: any) => {
               className="transition group-hover:fill-white"
             >
               <path
-                className="fill-[#233852] group-hover:fill-white transition"
+                className="fill-[#233852] transition group-hover:fill-white"
                 d="M20.3536 4.85477C20.5488 4.65951 20.5488 4.34293 20.3536 4.14767L17.1716 0.965687C16.9763 0.770425 16.6597 0.770425 16.4645 0.965687C16.2692 1.16095 16.2692 1.47753 16.4645 1.67279L19.2929 4.50122L16.4645 7.32965C16.2692 7.52491 16.2692 7.84149 16.4645 8.03675C16.6597 8.23202 16.9763 8.23202 17.1716 8.03675L20.3536 4.85477ZM0 5.00122H20V4.00122H0V5.00122Z"
               ></path>
             </svg>
@@ -39,11 +38,11 @@ const MediaTabLayout = ({ mediaData, pressData }: any) => {
         </div>
       </main>
       <section className="container">
-        <div className=" md:pb-[100px]">
+        <div className="md:pb-[100px]">
           {/* Tabs */}
-          <div className="flex justify-start border-b border-gray-300 mb-4">
+          <div className="mb-4 flex justify-start border-b border-gray-300">
             <button
-              className={`px-6 text-lg font-medium py-[25px] ${
+              className={`px-6 py-[25px] text-lg font-medium ${
                 activeTab === "news"
                   ? "border-b-2 border-blue-600 text-blue-600"
                   : "text-gray-500"
@@ -53,7 +52,7 @@ const MediaTabLayout = ({ mediaData, pressData }: any) => {
               In the News
             </button>
             <button
-              className={`px-6 text-lg font-medium py-[25px] ${
+              className={`px-6 py-[25px] text-lg font-medium ${
                 activeTab === "press"
                   ? "border-b-2 border-blue-600 text-blue-600"
                   : "text-gray-500"
@@ -67,35 +66,35 @@ const MediaTabLayout = ({ mediaData, pressData }: any) => {
           {/* Tab Content */}
           <div>
             {activeTab === "news" && (
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
                 {mediaData.map((card: any, index: Key) => (
                   <div
                     key={index}
-                    className="bg-white border rounded-none shadow-sm p-4"
+                    className="rounded-none border bg-white p-4 shadow-sm"
                   >
                     <a href={card.link} target="_blank">
                       <img
                         src={card.image ?? "/images/blogs/placeholder.jpg"}
                         alt={card.article_name}
-                        className="h-[220px] w-full object-cover mb-4"
+                        className="mb-4 h-[220px] w-full object-cover"
                       />
                       <div className="flex flex-col justify-between">
                         <h3
-                          className="text-lg font-semibold text-gray-800 line-clamp-2 min-h-[58px]"
+                          className="line-clamp-2 min-h-[58px] text-lg font-semibold text-gray-800"
                           dangerouslySetInnerHTML={{
                             __html: card.article_name ?? "",
                           }}
                         ></h3>
                         <div>
                           <p
-                            className="text-sm text-gray-600 line-clamp-3 min-h-[60px]"
+                            className="line-clamp-3 min-h-[60px] text-sm text-gray-600"
                             dangerouslySetInnerHTML={{
                               __html: card.meta_text ?? "",
                             }}
                           ></p>
-                          <div className="flex justify-between items-center mt-4 text-sm text-gray-500">
+                          <div className="mt-4 flex items-center justify-between text-sm text-gray-500">
                             <span>{card.date}</span>
-                            <span className="text-blue-600 font-medium">→</span>
+                            <span className="font-medium text-blue-600">→</span>
                           </div>
                         </div>
                       </div>
@@ -106,19 +105,19 @@ const MediaTabLayout = ({ mediaData, pressData }: any) => {
             )}
 
             {activeTab === "press" && (
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3">
                 {pressData.map((card: any, index: Key) => (
                   <a href={card.link} target="_blank">
                     <div
                       key={index}
-                      className="bg-white border rounded-none shadow-sm p-6 flex flex-col items-center justify-center min-h-[204px]"
+                      className="flex min-h-[204px] flex-col items-center justify-center rounded-none border bg-white p-6 shadow-sm"
                     >
-                      <div className="flex justify-between w-full items-center">
+                      <div className="flex w-full items-center justify-between">
                         <span className="text-[#37C4CD]">{card.date}</span>
                       </div>
 
                       <h3
-                        className="text-lg font-semibold text-gray-800 line-clamp-3 mt-4"
+                        className="mt-4 line-clamp-3 text-lg font-semibold text-gray-800"
                         dangerouslySetInnerHTML={{
                           __html: card.article_name ?? "",
                         }}
